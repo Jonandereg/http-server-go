1281d81a45a8d0ae7dda94234176f63a03beca24
jon-elosua_qonto <jon.elosua@qonto.com>
2026-03-11 17:49:40 +0100

    respond 200

--- app/main.go
+++ app/main.go
@@ -14,9 +14,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	resp := "HTTP/1.1 200 OK\\r\\n\\r\\n"
+	if _, err := conn.Write([]byte(resp)); err != nil {
+		fmt.Println("error writing to connection", err.Error())
+	}
+
 }
